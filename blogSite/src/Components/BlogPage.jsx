5d0f9b76ecfb45f4b84471e7e48e373438b71faa
akash <akashh889007@gmail.com>
2024-09-17 20:40:07 +0600

    pageination

--- blogSite/src/Components/BlogPage.jsx
+++ blogSite/src/Components/BlogPage.jsx
@@ -4,15 +4,26 @@ import BlogCards from './BlogCards';
 const BlogPage = () => {
 
     const [blogs, setBlogs] = useState([])
+    const [currentPage, setCurrentPage] = useState(1)
+    const pageSize = 12 /*Blog per page */
+    const [selectedCategory, setselectedCategory] = useState(null);
+
+
     useEffect( () => {
         const fetchBlogs = async () => {
-            let url = 'http://localhost:5000/blogs'
+            let url = `http://localhost:5000/blogs?page=${currentPage}&limit=${pageSize}`
+
+            // filter by category
+            if(selectedCategory) {
+              url+= `&category=${selectedCategory}`
+            }
+
             const response = await fetch(url)
             const data = await response.json()
             setBlogs(data)
         }
         fetchBlogs()
-    },[])
+    },[currentPage,pageSize,selectedCategory])
   return (
     <div>
       {/* catagory sections */}
